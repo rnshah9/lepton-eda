c48fc8175acfa63ed1062e062a8453b3647ac708
Raj Shah <rajster11442@gmail.com>
2022-05-25 17:34:51 +0000

    Integrate Mayhem

---
+++ version.h
@@ -0,0 +1,8 @@
+/* Date part of package version string. */
+#define PACKAGE_DATE_VERSION "20211219"
+
+/* Dotted part of package version string. */
+#define PACKAGE_DOTTED_VERSION "1.9.17"
+
+/* Current git commit. */
+#define PACKAGE_GIT_COMMIT "f843c30e09b3109724aaf6e8ba6d59a9f78fceb6"
